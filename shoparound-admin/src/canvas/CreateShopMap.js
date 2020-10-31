221d86f80766533d8db38db988bb22aac46fde25
IrinaChitu <chitu.irina.nicoleta@gmail.com>
2020-10-31 21:56:07 +0200

    Added canvas to create shop's map
    Added option to see which products are on a certain shelf
    
    Initial commit
    
    Created expo project

---
+++ shoparound-admin/src/canvas/CreateShopMap.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+  document.getElementById('productList').style.visibility = 'hidden';
+
+  var nrRow = 15;
+  var nrCol = 25;
+  var gridTable = document.createElement('TABLE');
+  for (var i = 0; i < nrRow; i++) {
+    var gridRow = document.createElement('TR');
+    for (var j = 0; j < nrCol; j++) {
+      var gridCell = document.createElement('TD');
+      var gridPlus = document.createElement('IMG');
+      gridPlus.src = '../../public/images/plusShelf.png';
+      gridPlus.id = `${i * nrCol + j}`;
+      gridPlus.addEventListener('click', addShelf);
+      gridPlus.classList.add('plus');
+      gridCell.classList.add('grid');
+      gridCell.appendChild(gridPlus);
+      gridRow.appendChild(gridCell);
+    }
+    gridTable.appendChild(gridRow);
+  }
+  document.getElementById('gridSpace').appendChild(gridTable);
+};
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData('text', ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData('text');
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function addShelf(ev) {
+  ev.target.src = '../../public/images/shelf.png';
+  document.getElementById('productList').style.visibility = 'visible';
+}
