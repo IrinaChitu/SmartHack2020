aee9ba92422c7e23e14027f1feea3c89a570ddc9
TooHappy22 <teodormarchitan@gmail.com>
2020-11-01 09:52:16 +0200

    Add firestore config

--- shoparound-db-api/app.js
+++ shoparound-db-api/app.js
@@ -2,13 +2,45 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
 
-app.get('/', function(req, res) {
-    res.send({ans: "NICE"});
+const app = require('firebase/app');
+require('firebase/auth');
+require('firebase/firestore');
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+app.initializeApp(config);
+db = app.firestore();
+
+app.get('/items', function(req, res) {
+    const itemsRef = db.collection('users')
+                    .doc(authUser)
+                    .collection('items');
+
+    const snapshot = await itemsRef.get();
+    if (snapshot.empty) {
+      res.send({code: 400});
+    }
+
+    let items = [];
+    snapshot.forEach((item) => {
+      const id = item.id;
+      const { name, description, category } = item.data();
+      items.push({ id, name, description, category });
+    });
+
+    res.send({items: items});
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
