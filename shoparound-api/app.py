dae21d5e28e36478862a4317b5afc13b4864e5b8
eu3neuom <manghiuc_florin@yahoo.com>
2020-11-01 03:00:34 +0200

    Implementing hamiltonian cycle

--- shoparound-api/app.py
+++ shoparound-api/app.py
@@ -1,25 +1,36 @@
 from flask import Flask, request, jsonify
+from lib import matrix_utils
+
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
+@app.route('/getimage/', methods=['POST'])
 def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+    rows_num = request.form.get("rows_num")
+    cols_num = request.form.get("cols_num")
+    start_pos = request.form.get("start_pos")
+    finish_pos = request.form.get("finish_pos")
+    shelves = request.form.get("shelves")
+    interest = request.form.get("interest")
+
+    matrix = matrix_utils.get_matrix(rows_num, cols_num, shelves)
+    total_cost, path = matrix_utils.solve_hamiltonian_path(start_pos, finish_pos, interest, matrix)
 
     # For debugging
-    print(f"Got this nice name {name} !!!!!")
+    # print(f"")
 
     response = {}
+    response["total_cost"] = total_cost
+    response["path"] = path
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+    # # Check if user sent a name at all
+    # if not name:
+    #     response["ERROR"] = "no name found, please send a name."
+    # # Check if the user entered a number not a name
+    # elif str(name).isdigit():
+    #     response["ERROR"] = "name can't be numeric."
+    # # Now the user entered a valid name
+    # else:
+    #     response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
 
     # Return the response in json format
     return jsonify(response)
@@ -40,10 +51,10 @@ def post_something():
             "ERROR": "no name found, please send a name."
         })
 
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+# # A welcome message to test our server
+# @app.route('/')
+# def index():
+#     return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
